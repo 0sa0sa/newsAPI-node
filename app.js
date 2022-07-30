5ec9ea2c96258816f86175bdbef98acdcaf9dfc7
osasou <osasou0125@gmail.com>
2022-07-30 10:48:05 +0900

    add newsapi package

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const cors = require("cors");
-const { request } = require("express");
 const app = express();
 const port = 8080;
+
+const NewsAPI = require("newsapi");
+const newsapi = new NewsAPI(process.env.APIKEY);
 
 app.use(cors());
 // const allowCrossDomain = function (req, res, next) {
@@ -21,16 +23,22 @@ app.use(cors());
 //   }
 // };
 // app.use(allowCrossDomain);
-const url =
-  "https://newsapi.org/v2/top-headlines?" +
-  "country=us&" +
-  `apiKey=${process.env.APIKEY}`;
+// const options = {
+//   url:
+//     "https://newsapi.org/v2/top-headlines?" +
+//     "country=us&" +
+//     `apiKey=${process.env.APIKEY}`,
+//   method: "GET",
+//   headers:
+//     "User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.0.0 Safari/537.36",
+// };
 
-app.get("/", (req, res) => {
-  request(url, (err, res, body) => {
-    console.log(body);
-    res.send(body);
+app.get("/", async (req, res) => {
+  const response = await newsapi.v2.topHeadlines({
+    country: "us",
   });
+  console.log("res ", response);
+  res.send(response);
 });
 
 // app.use(cors({ origin: "http://127.0.0.1:5173/" }));
